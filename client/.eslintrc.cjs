6f1f107a8c05a1fdff538d6641d40eb07863ecdf
Marcelo Claro <marc01mcl@hotmail.com>
2023-07-14 16:33:38 -0300

    chore: add update configuration

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -7,7 +7,6 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
-    '@rocketseat/eslint-config/react',
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },

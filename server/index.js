a34001d2ba1f88ee1fadcf209d233e62b33ddd89
Marcelo Claro <marc01mcl@hotmail.com>
2023-08-15 11:01:58 -0300

    update deployment

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ import dalleRoutes from './routes/dalle.routes.js'
 
 dotenv.config()
 
+const port = process.env.PORT || 3001
+
 const app = express()
 app.use(cors())
 app.use(express.json({ limit: '50mb' }))
@@ -16,6 +18,6 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello World!' })
 })
 
-app.listen(3333, () => {
-  console.log('Server is running on port 3333')
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
 })
